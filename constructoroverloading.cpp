982acea2b0652048704696376a6d60f2ffd24f7c
dfamrln <mhdffa1@gmail.com>
2026-05-04 12:02:52 +0700

    Implementasi class mahasiswa

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 class mahasiswa {
 private:
-
+    int nim;
+    string nama;
 public:
+    mahasiswa();
+    mahasiswa(int iNim);
+    mahasiswa(string iNama);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
