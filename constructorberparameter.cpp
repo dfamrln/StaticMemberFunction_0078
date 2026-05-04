90a0eaa5aaa42b5ee8761edd5d5725772d3a1d30
dfamrln <mhdffa1@gmail.com>
2026-05-04 12:08:37 +0700

    inisialisasi object mahasiswa dengan constructor

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 class mahasiswa {
-    
+public:
+    mahasiswa(); // Constructor
+    mahasiswa(int nim, string nama); // Constructor dengan parameter
+    };
+
+mahasiswa::mahasiswa() {
+    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
+}
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+int main(){
+    mahasiswa mhs; // Pembuatan Object
+    mahasiswa mhs2(12345, "Pascal"); // memanggil constructor dengan parameter
+    return 0;
 }
